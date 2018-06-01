a14611b87af878ce4a92df1f55a9c4010dc920eb
Anurag Kalra <anuragkalra@outlook.com>
2018-06-01 11:51:52 -0400

    deleted old files and js snippets

--- api-call-in-react-success.js
+++
@@ -1,5 +0,0 @@
-const url = 'https://api.blockchain.info/charts/market-price?format=json&cors=true';
-fetch(url).then(response => console.log(response), error => console.log(error));
-
-const url = 'https://api.blockchain.info/charts/transactions-per-second?timespan=5weeks&rollingAverage=8hours&format=json&cors=true'
-fetch(url).then(response => response.json()).then(r => console.log(r));
--- old/utility/api-usage/print-specific-value-from-stat-api.js
+++
@@ -1,8 +0,0 @@
-//npm install blockchain.info
-let statistics = require('blockchain.info/statistics');
-
-function printMarketPrice() {
-  statistics.get().then(response => console.log('Market price is: ' + response.market_price_usd));
-}
-
-printMarketPrice();
