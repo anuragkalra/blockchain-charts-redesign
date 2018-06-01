efdb08c1f2ddcbb395351e1b40436427fc7ba328
Anurag Kalra <anuragkalra@outlook.com>
2018-06-01 11:26:53 -0400

    refactored Market Capitalization chart component to handle network failure

--- main/src/components/MarketCapitalization.js
+++ main/src/components/MarketCapitalization.js
@@ -9,33 +9,37 @@ class MarketCapitalization extends React.Component {
         labels : [],
         datasets : [{
           label : 'market-cap',
+          borderColor: 'rgb(0, 74, 124, 0.8)',
+          backgroundColor: 'rgb(0, 74, 124, 0.2)',
           data : null
         }]
       }
     };
   }
 
-  componentDidMount() {
+  componentDidMount(){
     const url = 'https://api.blockchain.info/charts/market-cap?format=json&cors=true';
     fetch(url)
-      .then(response => response.json())
-      .then(result => {
-        let xValues = result.values.map(e => e.x.toString())
-        let yValues = result.values.map(e => e.y)
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      }).then(result => {
+        let xValues = result.values.map(e => e.x.toString());
+        let yValues = result.values.map(e => e.y);
         this.setState({
           chartData : {
             labels : xValues,
             datasets : [{
               label : 'market-cap',
-              borderColor: 'rgb(0, 74, 124, 0.8)',
-              backgroundColor: 'rgb(0, 74, 124, 0.2)',
               data : yValues
             }]
           }
         });
-
+      }).catch(error => {
+        console.log(error);
       });
-
   }
 
   render() {
